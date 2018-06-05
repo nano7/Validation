4e64fb65144db785b41a301d809244f1075e8631
Bruno Goncalves <bugotech@gmail.com>
2018-06-05 12:57:06 -0300

    add validation exception

--- src/ValidationException.php
+++ src/ValidationException.php
@@ -2,5 +2,34 @@
 
 class ValidationException extends \Exception
 {
+    /**
+     * @var array
+     */
+    protected $errors = [];
 
+    /**
+     * @param string $message
+     * @param null $code
+     * @param \Exception|null $previous
+     * @param array $errors
+     */
+    public function __construct($message, $code = null, $previous = null, array $errors = [])
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->errors;
+    }
+
+    /**
+     * @param null $key
+     * @return array|null
+     */
+    public function getErrors($key = null)
+    {
+        if (is_null($key)) {
+            return $this->errors;
+        }
+
+        return array_key_exists($key, $this->errors) ? $this->errors[$key] : null;
+    }
 }
