dd54af2eb5892677d4c9eeb7a75405d34bd5700f
Bruno Goncalves <bugotech@gmail.com>
2018-06-19 08:40:11 -0300

    usando ErrorException

--- src/ValidationException.php
+++ src/ValidationException.php
@@ -1,35 +1,7 @@
 <?php namespace Nano7\Validation;
 
-class ValidationException extends \Exception
+use Nano7\Foundation\Support\ErrorsException;
+
+class ValidationException extends ErrorsException
 {
-    /**
-     * @var array
-     */
-    protected $errors = [];
-
-    /**
-     * @param string $message
-     * @param null $code
-     * @param \Exception|null $previous
-     * @param array $errors
-     */
-    public function __construct($message, $code = null, $previous = null, array $errors = [])
-    {
-        parent::__construct($message, $code, $previous);
-
-        $this->errors = $errors;
-    }
-
-    /**
-     * @param null $key
-     * @return array|null
-     */
-    public function getErrors($key = null)
-    {
-        if (is_null($key)) {
-            return $this->errors;
-        }
-
-        return array_key_exists($key, $this->errors) ? $this->errors[$key] : null;
-    }
 }
